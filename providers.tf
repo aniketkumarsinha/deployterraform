b7ab6f2aa04bc83323bdc2979d9b803d016ba324
aniket-sinha8 <sinhaaniket32@gmail.com>
2025-02-15 13:14:38 +0530

    using tf version 1.10.5

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ terraform {
         version = "~> 4.14.0"
     }
   }
-  required_version = "~> 1.1.9"
+  required_version = "1.10.5"
 
   backend "azurerm" {
     resource_group_name  = "tfremotebackend"
