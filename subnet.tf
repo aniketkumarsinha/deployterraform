a4e8d6a7c3a283fd43115dacd1d5aeca7142e754
aniket-sinha8 <sinhaaniket32@gmail.com>
2025-02-15 13:41:41 +0530

    removed files

--- subnet.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
--- virtual-network.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
